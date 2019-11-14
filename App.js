b7e15490bbf5dd2d88d69837e70ebebb96cb45aa
Sithija Shehara <nsithijashehara@gmail.com>
2019-11-14 11:29:55 +0530

    implemented email_auth->Open.js

---
+++ App.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+} from 'react-native';
+
+import Open from './components/Open';
+
+
+
+export default function App() {
+
+  return (
+
+    <Open/>
+
+  );
+
+}
+
+
+
+const styles = StyleSheet.create({
+
+  container: {
+
+    flex: 1,
+
+    backgroundColor: '#fff',
+
+    alignItems: 'center',
+
+    justifyContent: 'center',
+
+  },
+
+});
