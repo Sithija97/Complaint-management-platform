8e4957b9caa844b3f393ea83deb0a3e2dc374fae
Sithija Shehara <nsithijashehara@gmail.com>
2021-05-05 21:19:45 +0530

    Seperated Todo component

--- homie/src/components/Todo/Todo.jsx
+++ homie/src/components/Todo/Todo.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import './Todo.css'
-
+import React from "react";
+import "./Todo.css";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import IconButton from '@material-ui/core/IconButton';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 function Todo({ text }) {
-    return (
-        <div></div>
-    )
+  return (
+    <div>
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <CheckCircleIcon color="primary" />
+          </ListItemIcon>
+          <ListItemText primary="primary" />
+          <ListItemSecondaryAction>
+            <IconButton edge="end" aria-label="edit">
+              <EditIcon />
+            </IconButton>
+            <IconButton edge="end" aria-label="delete">
+              <DeleteIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      </List>
+    </div>
+  );
 }
 
-export default Todo
+export default Todo;
--- homie/src/components/Todo/Todos.jsx
+++ homie/src/components/Todo/Todos.jsx
@@ -5,15 +5,6 @@ import {
     FormControl,
     TextField,
 } from "@material-ui/core";
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import IconButton from '@material-ui/core/IconButton';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import "./Todos.css";
 import Todo from "./Todo";
 
@@ -41,20 +32,7 @@ function Todos() {
                 </Button>
                 </Card>
             </form>
-            <List><ListItem button>
-                <ListItemIcon>
-                    <CheckCircleIcon color="primary" />
-                </ListItemIcon>
-                <ListItemText primary='primary' />
-                <ListItemSecondaryAction>
-                    <IconButton edge="end" aria-label="edit" >
-                        <EditIcon />
-                    </IconButton>
-                    <IconButton edge="end" aria-label="delete" >
-                        <DeleteIcon />
-                    </IconButton>
-                </ListItemSecondaryAction>
-            </ListItem></List>
+            <Todo/>
         </div>
     );
 }
